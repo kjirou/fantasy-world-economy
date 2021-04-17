60de8606700d7a8a8723d33278c96d11c5980e89
kjirou <sorenariblog@gmail.com>
2021-04-17 18:20:11 +0900

    Create a framework for the main loop

--- main.go
+++ main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+type Game struct {
+	worldTurnNumber int
+}
+
+type Human struct {
+	id      string
+	name    string
+	satiety int
+}
+
+func runMainLoop(game *Game) {
+	for {
+		interval := time.Microsecond * 16666
+		time.Sleep(interval)
+
+		game.worldTurnNumber = game.worldTurnNumber + 1
+
+		fmt.Println(game.worldTurnNumber)
+	}
+}
+
 func main() {
-	fmt.Println("Hello!")
+	rand.Seed(time.Now().UnixNano())
+
+	game := &Game{}
+
+	go runMainLoop(game)
+
+	fmt.Println("Start a server.")
+
+	time.Sleep(time.Second * 5)
 }
